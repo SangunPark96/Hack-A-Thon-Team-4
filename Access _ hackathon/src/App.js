245b084cffe73e6f167327238c8cb8f29b504207
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-07-22 14:51:36 -0400

    Add bootstrap style to 404 page

--- Access _ hackathon/src/App.js
+++ Access _ hackathon/src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css"; // BootStrap
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./Pages/NavBar";
--- Access _ hackathon/src/Pages/FourOFour.js
+++ Access _ hackathon/src/Pages/FourOFour.js
@@ -2,12 +2,16 @@ import { Link } from "react-router-dom";
 
 function FourOFour() {
   return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Oops! Page not found</h1>
-      <p>
-        The page you are looking for might have been removed or doesn't exist.
-      </p>
-      <Link to="/">Please Visit Homepage</Link>
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      <div className="text-center">
+        <h1 className="display-4">Oops! Page not found</h1>
+        <p className="lead">
+          The page you are looking for might have been removed or doesn't exist.
+        </p>
+        <Link to="/" className="btn btn-primary">
+          Please Visit Homepage
+        </Link>
+      </div>
     </div>
   );
 }
