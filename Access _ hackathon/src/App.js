628ee24cc2cf50dd354bc19930a0be2cbf0c033a
SangUn Park <Sangunpark1@gmail.com>
2023-07-22 14:30:10 -0400

    adding Routes, changing color to make things easier to read

--- Access _ hackathon/src/App.js
+++ Access _ hackathon/src/App.js
@@ -2,6 +2,8 @@ import logo from "./Assets/EcoLogo.ico"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./Pages/NavBar";
+import Footer from "./Pages/Footer";
+import FourOFour from "./Pages/FourOFour";
 
 function App() {
   return (
@@ -10,9 +12,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" /> 
         <Router>
           <NavBar/>
-          <Routes>
-            
-          </Routes>
+            <Routes>
+              <Route path="*" element={<FourOFour/>} />
+            </Routes>
+          <Footer/>
         </Router>
       </header>
     </div>
