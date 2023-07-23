45e3d37373f8abbaba0a491fe64f0fe38fdc99d1
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-07-23 11:42:19 -0400

    feedback finished with css

--- Access _ hackathon/src/Pages/Feedback.js
+++ Access _ hackathon/src/Pages/Feedback.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../Styles/Feedback.css";
 
 function Feedback() {
   const [rating, setRating] = useState(0);
@@ -7,7 +8,6 @@ function Feedback() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    // Process the feedback data here (e.g., send to backend, store in database)
     setSubmitted(true);
   }
 
@@ -19,11 +19,76 @@ function Feedback() {
       ) : (
         <form onSubmit={handleSubmit}>
           <div className="mb-3">
-            <label htmlFor="rating" className="form-label">
-              Rate from 1 to 5 stars:
-            </label>
-            {/* Star Rating Component (You can use a library or implement it yourself) */}
-            {/* Set the selected rating to the 'rating' state */}
+            <label className="form-label">
+              Please Rate from 1 to 5 stars:
+            </label>{" "}
+            <br />
+            <div className="form-check form-check-inline">
+              <input
+                type="radio"
+                name="rating"
+                id="star5"
+                value="5"
+                onChange={() => setRating(5)}
+                required // Make rating mandatory
+                className="form-check-input"
+              />
+              <label htmlFor="star5" className="form-check-label">
+                5 stars
+              </label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                type="radio"
+                name="rating"
+                id="star4"
+                value="4"
+                onChange={() => setRating(4)}
+                className="form-check-input"
+              />
+              <label htmlFor="star4" className="form-check-label">
+                4 stars
+              </label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                type="radio"
+                name="rating"
+                id="star3"
+                value="3"
+                onChange={() => setRating(3)}
+                className="form-check-input"
+              />
+              <label htmlFor="star3" className="form-check-label">
+                3 stars
+              </label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                type="radio"
+                name="rating"
+                id="star2"
+                value="2"
+                onChange={() => setRating(2)}
+                className="form-check-input"
+              />
+              <label htmlFor="star2" className="form-check-label">
+                2 stars
+              </label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                type="radio"
+                name="rating"
+                id="star1"
+                value="1"
+                onChange={() => setRating(1)}
+                className="form-check-input"
+              />
+              <label htmlFor="star1" className="form-check-label">
+                1 star
+              </label>
+            </div>
           </div>
           <div className="mb-3">
             <label htmlFor="comment" className="form-label">
@@ -34,8 +99,8 @@ function Feedback() {
               id="comment"
               value={comment}
               onChange={(event) => setComment(event.target.value)}
-              rows="4"
-              maxLength={200}
+              rows="3"
+              style={{ maxWidth: "300px", margin: "0 auto" }} // Adjust the width and center the text area
             />
           </div>
           <button type="submit" className="btn btn-primary">
