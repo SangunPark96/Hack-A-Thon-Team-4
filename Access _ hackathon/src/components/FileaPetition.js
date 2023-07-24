eb00b3bd5e7f088c8ed8738a2b6fc42de488f28f
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-07-24 09:32:20 -0400

    file a petition anonymous

--- Access _ hackathon/src/components/FileaPetition.js
+++ Access _ hackathon/src/components/FileaPetition.js
@@ -1,59 +1,100 @@
-import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import "../Styles/style.css"
+import React, { useState } from "react";
+import "../Styles/FileAPetition.css";
 
 const FileaPetition = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    petitionTitle: '',
-    petitionText: '',
+    name: "",
+    email: "",
+    petitionTitle: "",
+    petitionText: "",
+    anonymous: false, // New state for the checkbox
   });
 
   const [formSubmitted, setFormSubmitted] = useState(false);
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+    const { name, value, type, checked } = e.target;
+    const fieldValue = type === "checkbox" ? checked : value;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: fieldValue }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // If the user selected to submit anonymously, clear name and email
+    if (formData.anonymous) {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        name: "",
+        email: "",
+      }));
+    }
+
     // Perform form submission logic here, e.g., sending data to the backend
     console.log(formData);
-    // Clear the form after submission
     setFormData({
-      name: '',
-      email: '',
-      petitionTitle: '',
-      petitionText: '',
+      name: "",
+      email: "",
+      petitionTitle: "",
+      petitionText: "",
+      anonymous: false,
     });
-    // Set the formSubmitted state to true to close the form
     setFormSubmitted(true);
   };
 
   if (formSubmitted) {
-    return <p>Thank you for submitting the petition!</p>;
+    return (
+      <p className="thank-you-message">
+        Thank you for submitting the petition!
+      </p>
+    );
   }
 
   return (
     <div className="form-container">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="form-group" controlId="formEmail">
-          <Form.Label className="form-label">Email address</Form.Label>
-          <Form.Control
-            className="form-control"
-            type="email"
-            name="email"
-            value={formData.email}
+      <form onSubmit={handleSubmit}>
+        <div className="form-group form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            name="anonymous"
+            checked={formData.anonymous}
             onChange={handleChange}
-            placeholder="Enter your email"
           />
-        </Form.Group>
+          <label className="form-check-label">File Petition Anonymously</label>
+        </div>
 
-        <Form.Group className="form-group" controlId="formPetitionTitle">
-          <Form.Label className="form-label">Petition Title</Form.Label>
-          <Form.Control
+        {/* Render name and email fields only if not submitting anonymously */}
+        {!formData.anonymous && (
+          <React.Fragment>
+            <div className="form-group">
+              <label className="form-label">Name</label>
+              <input
+                className="form-control"
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Enter your name"
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="form-label">Email address</label>
+              <input
+                className="form-control"
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Enter your email"
+              />
+            </div>
+          </React.Fragment>
+        )}
+
+        <div className="form-group">
+          <label className="form-label">Petition Title</label>
+          <input
             className="form-control"
             type="text"
             name="petitionTitle"
@@ -61,27 +102,24 @@ const FileaPetition = () => {
             onChange={handleChange}
             placeholder="Enter the petition title"
           />
-        </Form.Group>
+        </div>
 
-        <Form.Group className="form-group" controlId="formPetitionText">
-          <Form.Label className="form-label">Petition Text</Form.Label>
-          <Form.Control
+        <div className="form-group">
+          <label className="form-label">Petition Text</label>
+          <textarea
             className="form-control form-textarea"
-            as="textarea"
             name="petitionText"
             value={formData.petitionText}
             onChange={handleChange}
             rows={4}
             placeholder="Enter the petition text"
           />
-        </Form.Group>
+        </div>
 
-        {/* Other form fields go here */}
-
-        <Button className="form-button" variant="primary" type="submit">
+        <button className="form-button" type="submit">
           Submit
-        </Button>
-      </Form>
+        </button>
+      </form>
     </div>
   );
 };
