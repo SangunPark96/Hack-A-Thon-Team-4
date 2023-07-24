6f1004567880b3692ff8856d63ac68358ee175e7
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-07-24 09:56:54 -0400

    remove stats page from home

--- Access _ hackathon/src/Pages/Home.js
+++ Access _ hackathon/src/Pages/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
-import PetitionStatistics from "../components/PetitionStatistics";
 import PopularPetitions from "../components/PopularPetition";
 
 const Home = () => {
   return (
     <div className="d-flex flex-column align-items-center justify-content-center">
       <div className="text-center">
-        <PetitionStatistics />
         <PopularPetitions />
       </div>
     </div>
