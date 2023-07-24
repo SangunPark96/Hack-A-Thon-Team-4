13b0dfe8a4cb317c4bbaa78f20cd1cd7044967de
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-07-24 11:24:14 -0400

    edit council.css

--- Access _ hackathon/src/components/CityCouncil.js
+++ Access _ hackathon/src/components/CityCouncil.js
@@ -1,43 +1,53 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CouncilorIndex from "./CouncilorIndex";
 
-
 export default function CityCouncil() {
+  const [repsList, setRepsList] = useState([]);
+  const [borough, setBorough] = useState("");
 
-    const [ repsList, setRepsList] = useState([])
-    const [ borough, setBorough ] = useState("")
+  useEffect(() => {
+    axios
+      .get(`https://data.cityofnewyork.us/resource/uvw5-9znb.json`)
+      .then((res) => {
+        setRepsList(res.data);
+      })
+      .catch((e) => {
+        console.log({ error: { e } });
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get(`https://data.cityofnewyork.us/resource/uvw5-9znb.json`).then((res) => {
-            setRepsList(res.data)
-        }).catch((e) => {
-            console.log({ error: {e}})
-        })
-    }, []);
+  const handleChange = (event) => {
+    setBorough(event.target.value);
+  };
 
-    const handleChange = (event) => {
-        setBorough(event.target.value);
-      };
-    
-
-    return (
-        <div class="container-fluid">
-            <label for="boroughs"><strong>Select your borough:</strong></label>
-
-                <select name="boroughs" class="form-control" id="borough" onChange={handleChange}>
-                    <option value="placeholder">Choose a Borough in New York City</option>
-                    <option value="Bronx">Bronx</option>
-                    <option value="Queens">Queens</option>
-                    <option value="Brooklyn">Brooklyn</option>
-                    <option value="Manhattan">Manhattan</option>
-                    <option value="Staten Island">Staten Island</option>
-                </select>   
-            <div>
-                
-                    <CouncilorIndex councilors={repsList} borough={borough} />
-                
-            </div>
+  return (
+    <div className="container-fluid">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <label htmlFor="boroughs">
+            <strong>Select your borough:</strong>
+          </label>
+          <select
+            name="boroughs"
+            className="form-control"
+            id="borough"
+            onChange={handleChange}
+          >
+            <option value="placeholder">
+              Choose a Borough in New York City
+            </option>
+            <option value="Bronx">Bronx</option>
+            <option value="Queens">Queens</option>
+            <option value="Brooklyn">Brooklyn</option>
+            <option value="Manhattan">Manhattan</option>
+            <option value="Staten Island">Staten Island</option>
+          </select>
         </div>
-    )
+      </div>
+      <div className="mt-4">
+        <CouncilorIndex councilors={repsList} borough={borough} />
+      </div>
+    </div>
+  );
 }
--- Access _ hackathon/src/components/Councilor.js
+++ Access _ hackathon/src/components/Councilor.js
@@ -1,14 +1,17 @@
-import "./Councilor.css"
+import React from "react";
+import "../Styles/Councilor.css";
 
-export default function Councilor({reps}){
-    return(
-        <div>
-                <div className="RepMap">
-                    <h3>{reps.name}</h3>
-                    <br></br>
-                    <strong>District Number:</strong><p>{reps.district}</p>
-                   <strong>Political Party:</strong><p>{reps.political_party}</p>
-                </div>
-        </div>
-    )
+export default function Councilor({ reps }) {
+  return (
+    <div>
+      <div className="RepMap">
+        <h3>{reps.name}</h3>
+        <br />
+        <strong>District Number:</strong>
+        <p>{reps.district}</p>
+        <strong>Political Party:</strong>
+        <p>{reps.political_party}</p>
+      </div>
+    </div>
+  );
 }
