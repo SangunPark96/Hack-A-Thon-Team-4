92a58d35ab8d8c222f8b2df8ae619af94481821b
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-07-24 11:53:26 -0400

    edit council.css

--- Access _ hackathon/src/components/CityCouncil.js
+++ Access _ hackathon/src/components/CityCouncil.js
@@ -66,7 +66,7 @@ export default function CityCouncil() {
         </div>
         <div className="col-md-6">
           <div className="row justify-content-center">
-            <div className="col-md-6">
+            <div className="col-md-10">
               <input
                 type="text"
                 className="form-control"
